4258917e1bad956de20b75398b365931c636fd9c
Lorent Sinani <lorent.sinani0@gmail.com>
2024-12-20 00:41:12 +0100

    initial version of clause extraction and tbd logic for question_answering

---
+++ js/app/clause_extraction.js
@@ -0,0 +1,9 @@
+const nlp = require('compromise');
+
+function extractEntities(text) {
+    const doc = nlp(text);
+    const entities = doc.topics().out('array');
+    return entities;
+}
+
+module.exports = { extractEntities };
